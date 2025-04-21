5ddc0be7d6b2fa874b3512993ad6b8861970a77c
rizkym1 <rizkimaulana3216@gmail.com>
2025-04-21 15:58:05 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+    ],
+
+    safelist: [
+        "w-20",
+        "h-20",
+        "text-gray-500", // untuk logo Laravel
+        "text-center",
+        "text-sm",
+        "text-gray-600",
+        "font-medium", // class umum yang sering hilang
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
